d11556b7d8f468a67ac4321c5dfa8fc70596a4bf
Christopher Campbell <campbefs@gmail.com>
2021-07-08 12:30:42 -0700

    resolved issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import Nav from './components/Nav';
 import Main from './components/Main';
@@ -8,20 +9,42 @@ import Home from './components/Home';
 import Profile from './components/Profile';
 import LikedPost from './components/LikedPosts';
 import RecipeSearch from './components/RecipeSearch'
-import Post from './components/Post'
+import Post from './components/Post';
+
+import { ApolloProvider } from '@apollo/client';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: 'http://localhost:3001/graphql'
+  // uri: '/graphql'
+});
+
 
 function App() {
   const pages = [<Main/>, <Home/>, <Profile/>, <LikedPost/>, <Login/>, <SignUp/>];
   const [page, setPage] = useState(pages[0]);
 
   return (
+    <ApolloProvider client={client}>
+      <Router>
     <div className='App'>
-      <RecipeSearch/>
-      {/* <Nav setPage={setPage} pages={pages}/>
-      {page} */}
+      {<Nav setPage={setPage} pages={pages}/>}
+      {page}
       {/* <RecipeSearch/> */}
 
-      </div>
+    </div>
+      </Router>
+ </ApolloProvider>
   );
 }
 
